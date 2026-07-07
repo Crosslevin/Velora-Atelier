40c57ad97278c4584ff29b154f5317724aae320b
Crosslevin <hellolevincross@gmail.com>
2026-07-07 16:38:41 +0530

    Implement user authentication functions

---
+++ user-auth.js
@@ -0,0 +1,324 @@
+// ==========================================
+// VELORA ATELIER
+// user-auth.js
+// Customer Authentication
+// ==========================================
+
+
+
+import {
+
+createUserWithEmailAndPassword,
+
+signInWithEmailAndPassword,
+
+signOut,
+
+onAuthStateChanged
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+
+import {
+
+doc,
+
+setDoc
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+
+import {
+
+auth,
+
+db
+
+}
+
+from "./firebase-config.js";
+
+
+
+
+
+// =============================
+// USER SIGNUP
+// =============================
+
+
+async function signupUser(
+
+name,
+
+email,
+
+password
+
+){
+
+
+try{
+
+
+const result =
+
+await createUserWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+const user =
+result.user;
+
+
+
+await setDoc(
+
+doc(
+
+db,
+
+"users",
+
+user.uid
+
+),
+
+{
+
+
+name:name,
+
+
+email:email,
+
+
+createdAt:
+
+new Date()
+
+
+}
+
+);
+
+
+
+alert(
+"Account Created Successfully"
+);
+
+
+
+window.location.href =
+"shop.html";
+
+
+
+}
+
+
+catch(error){
+
+
+alert(
+error.message
+);
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+// =============================
+// USER LOGIN
+// =============================
+
+
+async function loginUser(
+
+email,
+
+password
+
+){
+
+
+try{
+
+
+const result =
+
+await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+localStorage.setItem(
+
+"userEmail",
+
+result.user.email
+
+);
+
+
+
+window.location.href =
+"shop.html";
+
+
+
+}
+
+
+catch(error){
+
+
+alert(
+"Invalid Login Details"
+);
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+// =============================
+// LOGOUT
+// =============================
+
+
+async function logoutUser(){
+
+
+await signOut(auth);
+
+
+localStorage.removeItem(
+"userEmail"
+);
+
+
+
+window.location.href =
+"login.html";
+
+
+
+}
+
+
+
+
+
+
+
+// =============================
+// SESSION CHECK
+// =============================
+
+
+onAuthStateChanged(
+
+auth,
+
+(user)=>{
+
+
+if(user){
+
+
+console.log(
+
+"User Active:",
+
+user.email
+
+);
+
+
+
+}
+
+
+else{
+
+
+console.log(
+"No User Login"
+);
+
+
+}
+
+
+
+});
+
+
+
+
+
+
+
+export {
+
+
+signupUser,
+
+loginUser,
+
+logoutUser
+
+
+};
+
+
+
+console.log(
+"User Authentication Ready ✔"
+);
